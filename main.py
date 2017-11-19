a49bb109bf84e1d7821b6c16bc127e2d5b5d9e97
Gary Song <garysong23@gmail.com>
2017-11-18 19:28:18 -0500

    add arguements

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+import sys
 import sqlite3
+import datetime
 
-MAIN_QUERY = ('''
-  SELECT
-    count(*) as count
-    , date(created_at,'-7 days', 'weekday 1') as week
-    , workflow_state as workflow_state
-  FROM applicants
-  GROUP BY strftime('%W', created_at), workflow_state
-  ORDER BY week ASC
-''')
+def build_query(begin_date, end_date):
+  query = ('''
+    SELECT
+      count(*) as count
+      , date(created_at,'-7 days', 'weekday 1') as week
+      , workflow_state as workflow_state
+    FROM applicants
+    WHERE (
+      created_at > '{}'
+      AND created_at < '{}'
+    )
+    GROUP BY strftime('%W', created_at), workflow_state
+    ORDER BY week ASC
+  ''').format(begin_date, end_date)
+  print(query)
+  return query
 
-def main():
+def excute_query(query):
   db = sqlite3.connect('applicants.sqlite3')
-  print('Database opened')
-
-  cursor = db.execute(MAIN_QUERY)
+  cursor = db.execute(query)
 
   with open('output.txt','w+') as f:
     names = list(map(lambda d: str(d[0]), cursor.description))
@@ -24,8 +31,21 @@ def main():
       row_str = ', '.join([str(i) for i in row]) + '\n'
       f.write(row_str)
 
-  print('Database closed')
   db.close()
+
+def main():
+  if (len(sys.argv) != 3):
+    print('Invalid arguments')
+    return
+
+  begin_time_str = sys.argv[1]
+  end_time_str = sys.argv[2]
+
+  begin_date = datetime.datetime.strptime(begin_time_str, '%Y-%m-%d')
+  end_date = datetime.datetime.strptime(end_time_str, '%Y-%m-%d')
+
+  query = build_query(begin_date, end_date)
+  excute_query(query)
 
 if __name__ == '__main__':
 	main()
